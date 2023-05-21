1e50eca3f373eb3ec07b078ccdc79ffee96f6cf9
NoePodmoguilny <npodmoguilny@gmail.com>
2023-05-21 16:29:34 -0300

    version 2.1. funcionalidades de formulario

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,55 +1,61 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+
 
 const Contact = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Lógica para enviar el formulario de contacto
-    // Utiliza los valores de name, email y message
-  };
+  
+  const [state, handleSubmit] = useForm("mnqyyqne");
+    if (state.succeeded) {
+        return <p className='container lightColor title d-flex flex-column align-items-center parrForm' >¡Gracias por tu mensaje!<span className='title'> Me pondré en contacto pronto</span></p>;
+    }
 
   return (
     <div className="container">
-      <div className="row mt-4">
+      <div className="row marginForm">
         <div className="col-md-6 offset-md-3">
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="form-control m-1"
-                placeholder="Name"
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="form-control m-1"
-                placeholder="Email"
-              />
-            </div>
-            <div className="form-group">
-              <textarea
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                className="form-control m-1"
-                placeholder="Message"
-              ></textarea>
-            </div>
-            <button type="submit" className="btn btnCV m-1">
-              Submit
-            </button>
-          </form>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <input
+                  id="email"
+                  type="email" 
+                  name="email"
+                  className="form-control m-1 paddingForm"
+                  placeholder="Email"
+                />
+                <ValidationError 
+                  prefix="Email" 
+                  field="email"
+                  errors={state.errors}
+                />
+              </div>
+              <div className="form-group">
+                <textarea
+                  id="message"
+                  name="message"
+                  className="form-control m-1 paddingForm"
+                  rows={'5'}
+                  placeholder="Message"
+                />
+                <ValidationError 
+                  prefix="Message" 
+                  field="message"
+                  errors={state.errors}
+                />
+              </div>
+              <div className="form-group">
+                <button 
+                  type="submit" 
+                  disabled={state.submitting}
+                  className="form-control m-1 btnCV"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Contact;

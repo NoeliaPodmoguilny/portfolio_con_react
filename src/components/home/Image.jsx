4dd64dad10efbb1e2e2f64e7b1e077abacca8302
NoePodmoguilny <npodmoguilny@gmail.com>
2023-08-03 16:32:08 -0300

    image responsive

--- src/components/home/Image.jsx
+++ src/components/home/Image.jsx
@@ -2,12 +2,20 @@
 
 export const Image = () => {
     return (
-        <div className="col-md-6">
-            <img
-                src="/assets/pic.png"
-                alt="Foto de Noelia"
-                className="photo"
-            />
+        // <div className="col-md-6 img-fluid d-none d-lg-block">
+        //     <img
+        //         src="/assets/pic.png"
+        //         alt="Foto de Noelia"
+        //         className="photo"
+        //     />
+        // </div>
+        <div class="container">
+            <div class="d-none d-lg-block">
+                <img src="/assets/pic.png" alt="Imagen de Noelia hecha con IA" class="img-fluid photo"/>
+            </div>
+            <div class="d-lg-none fs3 lightColor text-center">
+                <p>Noelia Podmoguilny</p>
+            </div>
         </div>
     )
 }

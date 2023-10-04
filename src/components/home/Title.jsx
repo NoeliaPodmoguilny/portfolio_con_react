5d94e1fc14d2e220745cae13018c6efa3a76909b
NoePodmoguilny <npodmoguilny@gmail.com>
2023-10-04 20:56:12 -0300

    change title

--- src/components/home/Title.jsx
+++ src/components/home/Title.jsx
@@ -3,7 +3,7 @@
 export const Title = () => {
     return (
         <div className='d-flex justify-content-center mt-4 animate__animated animate__slideInLeft'>
-            <h1 className='bg-transparent mainTitle'><span className='redColor'>{'<'}</span><span className='lightBlueColor'>Front-EnD</span>eveloper/<span className='redColor'>{'>'}</span></h1>
+            <h1 className='bg-transparent mainTitle'><span className='redColor'>{'<'}</span><span className='lightBlueColor'> Web</span>Developer /<span className='redColor'>{'>'}</span></h1>
         </div>
     )
 }
--- src/components/home/skillsItems/dataSkills.js
+++ src/components/home/skillsItems/dataSkills.js
@@ -1,56 +1,56 @@
 
 
 export const dataSkills = [
-    { 
-        text: 'HTML5', 
-        style: 'animate__slow 2s fs2' 
+    {
+        text: 'HTML5',
+        style: 'animate__slow 2s fs2'
     },
-    { 
-        text: 'CSS', 
-        style: 'animate__slower 3s fs3' 
+    {
+        text: 'CSS',
+        style: 'animate__slower 3s fs3'
     },
-    { 
-        text: 'JAVASCRIPT', 
-        style: 'fs1' 
+    {
+        text: 'JAVASCRIPT',
+        style: 'fs1'
     },
-    { 
-        text: 'BOOTSTRAP', 
-        style: 'animate__slow 2s fs2' 
+    {
+        text: 'BOOTSTRAP',
+        style: 'animate__slow 2s fs2'
     },
-    { 
-        text: 'TAILWIND', 
-        style: 'animate__slower 3s fs3' 
+    {
+        text: 'TAILWIND',
+        style: 'animate__slower 3s fs3'
     },
-    { 
-        text: 'GIT', 
-        style: 'animate__slow 2s fs2' 
+    {
+        text: 'GIT',
+        style: 'animate__slow 2s fs2'
     },
-    { 
-        text: 'GITHUB', 
-        style: 'animate__slower 3s fs3' 
+    {
+        text: 'GITHUB',
+        style: 'animate__slower 3s fs3'
     },
-    { 
-        text: 'SCRUM', 
-        style: 'animate__slow 2s fs2' 
+    {
+        text: 'SCRUM',
+        style: 'animate__slow 2s fs2'
     },
-    { 
-        text: 'SQL', 
-        style: 'fs1' 
+    {
+        text: 'SQL',
+        style: 'fs1'
     },
-    { 
-        text: 'ANGULAR', 
-        style: 'animate__slow 2s fs2' 
+    {
+        text: 'ANGULAR',
+        style: 'animate__slow 2s fs2'
     },
-    { 
-        text: 'TYPESCRIPT', 
-        style: 'animate__slow 2s fs2' 
+    {
+        text: 'PYTHON',
+        style: 'animate__slow 2s fs2'
     },
-    { 
-        text: 'REACT', 
-        style: 'fs1' 
+    {
+        text: 'REACT',
+        style: 'fs1'
     },
-    { 
-        text: 'JAVA', 
-        style: 'animate__slower 3s fs3' 
+    {
+        text: 'JAVA',
+        style: 'animate__slower 3s fs3'
     },
 ];
